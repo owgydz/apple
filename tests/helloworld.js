fd236df50453a5ffdb75db1923925c02bdebd37f
Owen Gaydosz <owgydz@proton.me>
2025-02-02 00:50:01 -0500

    Update helloworld.js

--- tests/helloworld.js
+++ tests/helloworld.js
@@ -1,3 +1,4 @@
 // Copyright 2025 the Apple authors
 // This code is governed by the Mozilla Public License v2.0. See in the LICENSE file.
+// NOTE: Testing files only. Not a part of the source. 
 console.log(`Hello world`)
