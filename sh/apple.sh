0ad7251564014516b6a1ca65228d5c35fa5910b0
Owen Gaydosz <owgydz@proton.me>
2025-02-02 16:22:53 -0500

    bash: Import apple.sh to install Apple with one bash script

---
+++ sh/apple.sh
@@ -0,0 +1,13 @@
+# Copyright 2025 the Apple authors.
+# This project is governed by the Mozilla Public License 2.0. View in the LICENSE file.
+# To make this script work, run `chmod +x apple/sh/apple.sh`.
+#!/bin/bash
+APPLE_DIR="$HOME/apple"
+
+# check as a fallback
+if [ ! -d "$APPLE_DIR" ]; then
+    APPLE_DIR="/root/apple"
+fi
+
+# run the thing
+node "$APPLE_DIR/js/apprepl.js" "$@"
