4ad141fb4413b40fe3117b589759f8459e15536e
Owen Gaydosz <owgydz@proton.me>
2025-02-02 00:50:49 -0500

    Update confl.js

--- tests/confl.js
+++ tests/confl.js
@@ -1,4 +1,5 @@
-
+// Copyright 2025 the Apple authors
+// This code is governed by the Mozilla Public License v2.0. See in the LICENSE file.
 // NOTE: Testing files only. Not a part of the source. 
 import { VariableDeclaration, IfStatement, WhileStatement, BinaryExpression, Literal, Identifier } from '../src/appast';
 import BytecodeCompiler from '../src/appvm';
