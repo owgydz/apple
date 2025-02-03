199123da54dcdb9e7935905bfa5c5fa686a28581
Owen Gaydosz <owgydz@proton.me>
2025-02-03 11:19:43 -0500

    Update apple.sh

--- sh/apple.sh
+++ sh/apple.sh
@@ -1,6 +1,10 @@
 # Copyright 2025 the Apple authors.
 # This project is governed by the Mozilla Public License 2.0. View in the LICENSE file.
 # To make this script work, run `chmod +x apple/sh/apple.sh`.
+# Then add it to your PATH.
+
+# Note that you can run the Apple command flags with this file.
+
 #!/bin/bash
 APPLE_DIR="$HOME/apple"
 
