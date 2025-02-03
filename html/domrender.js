32561992def10243d124455ae4074532264fffa1
Owen Gaydosz <owgydz@proton.me>
2025-02-02 21:12:49 -0500

    Add notice up top

--- html/domrender.js
+++ html/domrender.js
@@ -1,3 +1,6 @@
+// Copyright 2025 the Apple authors.
+// This project is governed by the Mozilla Public License 2.0. View in the LICENSE file.
+
 export function renderDOM(domElements) {
   return domElements.map((element) => {
     let html = `<${element.tagName}`;
