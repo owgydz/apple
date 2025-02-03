00c0d93424b68a505596a250a136f65f9180b1d8
Owen Gaydosz <owgydz@proton.me>
2025-02-02 21:27:08 -0500

    Update render.js --> v0.0.260

--- browser/render.js
+++ browser/render.js
@@ -1,7 +1,6 @@
 // Copyright 2025 the Apple authors.
 // This project is governed by the Mozilla Public License, v2.0. View in the LICENSE file.
-import Graphics from './graphics';
-import { VirtualDOM } from './virtualDOM';
+import Graphics from './graphics';  
 
 class Renderer {
   constructor() {
@@ -9,18 +8,23 @@ class Renderer {
   }
 
   render(virtualDOM) {
-    this.graphics.clear();]
-    
-    if (virtualDOM.type === 'div') {
-      this.graphics.drawRect(virtualDOM.x, virtualDOM.y, virtualDOM.width, virtualDOM.height, virtualDOM.style.backgroundColor || { r: 255, g: 255, b: 255, a: 255 });
-      if (virtualDOM.children) {
-        virtualDOM.children.forEach(child => this.render(child)); 
-      }
-    } else if (virtualDOM.type === 'text') {
-      this.graphics.renderText(virtualDOM.content, virtualDOM.x, virtualDOM.y);
-    }
-
+    this.graphics.clear();
+    virtualDOM.forEach((node) => this.renderNode(node));
     this.graphics.present();
+  }
+
+  renderNode(node) {
+    if (node.type === 'div') {
+      // Just draw a rectangle
+      this.graphics.drawRect(node.x, node.y, node.width || 100, node.height || 100, { r: 255, g: 255, b: 255, a: 255 });
+    } else if (node.type === 'text') {
+      // text
+      this.graphics.renderText(node.content, node.x, node.y);
+    }
+    
+    if (node.children) {
+      node.children.forEach((child) => this.renderNode(child));
+    }
   }
 
   close() {
