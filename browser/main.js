a428c7ba2802434f25c6399bc0b46ac62aa2a62c
Owen Gaydosz <owgydz@proton.me>
2025-02-03 10:35:29 -0500

    Add .js ends on the IMPORT functions

--- browser/main.js
+++ browser/main.js
@@ -1,9 +1,9 @@
 // Copyright 2025 the Apple authors.
 // This project is governed by the Mozilla Public License, v2.0. View in the LICENSE file.
 
-import { openGoogleWindow } from './window';
-import { fetchGoogleHomepage } from './google';  
-import Renderer from './render';  
+import { openGoogleWindow } from './window.js';
+import { fetchGoogleHomepage } from './google.js';  
+import Renderer from './render.js';  
 
 async function initBrowser() {
   const renderer = new Renderer();
