16dca028f769aab498837fae345c25e88d2ed8c4
Owen Gaydosz <owgydz@proton.me>
2025-02-03 10:34:49 -0500

    browser - main: Update main.js to use window.js

--- browser/main.js
+++ browser/main.js
@@ -1,6 +1,7 @@
 // Copyright 2025 the Apple authors.
 // This project is governed by the Mozilla Public License, v2.0. View in the LICENSE file.
 
+import { openGoogleWindow } from './window';
 import { fetchGoogleHomepage } from './google';  
 import Renderer from './render';  
 
@@ -8,7 +9,8 @@ async function initBrowser() {
   const renderer = new Renderer();
   const virtualDOM = await fetchGoogleHomepage();
   renderer.render(virtualDOM);
+  openGoogleWindow();
 }
 
 initBrowser();
-// On caffeine 
+// On caffeine
