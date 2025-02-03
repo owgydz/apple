60d73b2187b1ac49c55929869159557a13ba9d91
Owen Gaydosz <owgydz@proton.me>
2025-02-02 21:31:21 -0500

    Add copyright notice

--- browser/main.js
+++ browser/main.js
@@ -1,3 +1,6 @@
+// Copyright 2025 the Apple authors.
+// This project is governed by the Mozilla Public License, v2.0. View in the LICENSE file.
+
 import { fetchGoogleHomepage } from './google';  
 import Renderer from './render';  
 
