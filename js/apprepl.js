a344847ec1befba9805b5ad0231beb367ba6f73e
Owen Gaydosz <owgydz@proton.me>
2025-02-05 12:57:52 -0500

    Rollback version to v0.60.4

--- js/apprepl.js
+++ js/apprepl.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import readline from 'readline';
 import { spawnSync } from 'child_process';
 import { parseHTML, renderHTML, manipulateHTML } from '../html/src/html.js';
-const VERSION = "0.72.7";  
+const VERSION = "0.60.4";  
 
 const args = process.argv.slice(2);
 
